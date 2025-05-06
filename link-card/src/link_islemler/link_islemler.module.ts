f6adf246d564c7c9c289105fbc2c0d7b106dbde4
Denizhan Şahin <denizhansahin@yaani.com>
2025-05-07 00:51:52 +0300

    deniz

--- link-card/src/link_islemler/link_islemler.module.ts
+++ link-card/src/link_islemler/link_islemler.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
 import { LinkIslemlerService } from './link_islemler.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Kullanici } from 'src/Entities/kullanici.entity';
+import { KisiselLink } from 'src/Entities/kisiselLink.entity';
+import { KurumsalLink } from 'src/Entities/kurumsalLink.entity';
+import { Link } from 'src/Entities/kisalink.entity';
 
 @Module({
-  providers: [LinkIslemlerService]
+  providers: [LinkIslemlerService],
+  imports:[
+      TypeOrmModule.forFeature([Kullanici,KisiselLink,KurumsalLink,Link]),
+    ]
 })
 export class LinkIslemlerModule {}
